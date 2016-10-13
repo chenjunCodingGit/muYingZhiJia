8c6195fe71c12acc185fe666ea0a86b766ec8eeb
chenjunCodingGit <271676725@qq.com>
2016-10-13 09:33:51 +0800

    更新注册

---
+++ js/register.js
@@ -0,0 +1,6 @@
+/*空白搜索栏*/
+$.get("../data/register/login-blank.json",function(data){
+	$(".nav-blank>img").attr({src:data[0].src});//得到src
+	$(".nav-blank>img").attr({alt:data[0].alt})//得到alt
+	//console.log(data[0])
+})
