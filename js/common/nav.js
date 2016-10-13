9f4f4cdebe1780ed0a02d251c96cf1b3bed8bb72
chenjunCodingGit <271676725@qq.com>
2016-10-13 23:56:16 +0800

    添加商品详情目录

---
+++ js/common/nav.js
@@ -0,0 +1,8 @@
+/*直邮专区*/
+$('.nav-post-dd').mouseover(function(){
+	$('.nav-europe').css("display","block")
+})
+$('.nav-post-dd').mouseout(function(){
+	$('.nav-europe').css("display","none")
+})
+
