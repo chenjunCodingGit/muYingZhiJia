f34b1291343f59b791800f38b12c481461f48e2f
chenjunCodingGit <271676725@qq.com>
2016-10-13 21:06:45 +0800

    增加商品列表页面

---
+++ js/goodList.js
@@ -0,0 +1,23 @@
+/*点击切换商品列表页面*/
+$('.list-pre').click(function(){
+	$('.goodlist01').css("display","block")//切换页面
+	$('#list-click-btn').html("1")//显示当前页页码
+	$('.goodlist02').css("display","none")//切换页面
+	$('#list-wrap-div02').removeClass("click-color-active")//移除另外一个class
+	$('#list-wrap-div01').addClass("click-color-active")//添加当前一个class
+})
+$('.list-next').click(function(){
+	$('.goodlist02').css("display","block")//切换页面
+	$('#list-click-btn').html("2"); //显示当前页页码
+	$('.goodlist01').css("display","none")//切换页面
+	$('#list-wrap-div01').removeClass("click-color-active")//移除另外一个class
+	$('#list-wrap-div02').addClass("click-color-active")//添加当前一个class
+})
+$('.list-click-tab').each(function(){
+	$(this).click(function(){
+		$(this).addClass("click-color-active").siblings().removeClass("click-color-active");
+		$('#list-click-btn').html($(this).html())
+		console.log($(this).eq(0))
+		$('.goodlist0'+$(this).html()).css("display","block").siblings().css("display","none")
+	})
+})
