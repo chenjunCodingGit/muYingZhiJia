ed30e1bdbd835be086de9a9e1a316aa0b08c5e0e
chenjunCodingGit <271676725@qq.com>
2016-10-17 21:19:41 +0800

    添加轮播和三级导航

--- js/common/nav.js
+++ js/common/nav.js
@@ -6,3 +6,65 @@ $('.nav-post-dd').mouseout(function(){
 	$('.nav-europe').css("display","none")
 })
 
+/*去边框*/
+/*$('.nav02').mouseover(function(){
+	$(this).css("border-right","none");
+	$(this).siblings().css("border-right","1px solid #FF7700")
+})*/
+
+/*三级导航显示隐藏操作*/
+/*$('#ui-nav').mouseover(function(){
+	$('.nav02-ul').css("display","block");
+})
+//从二级导航移到三级导航
+$('.nav02').mouseover(function(){
+	$('.nav02-ul').css("display","block");
+	$(this).children().eq(2).css("display","block");
+	//移除第三级导航消失二三级导航
+	$(this).children().eq(2).mouseout(function(){
+		$(this).css("display","none");
+		$('.nav02-ul').css("display","none");
+	})
+})
+//移除二级导航还没移到三级导航，消失二级导航
+$('.nav02-ul').mouseout(function(){
+	$(this).css("display","none");
+})*/
+
+/*动态解析三级导航内容*/
+$.get("../data/common/nav/nav-third.json",function(data){
+	var a = 1;
+	for(var i in data){
+		if(i == "nav0"+a){
+			var b = 1;
+			for(var j in data[i]){
+				if(j == "box0"+b){
+					var c = 1;
+					for(var k in data[i][j]){
+						if(k == "text0"+c){
+							c++;
+							for(var m in data[i][j][k]){
+								console.log(data[i][j][k][m])
+								/*var aCont = $('<a href="###"></a>').html(data[i][j][k][m]);*/
+								console.log(c)
+								
+								/*$('.nav03').eq(c-1).append(aCont);*/
+							}
+						}
+						
+					}
+				}
+				b++;
+			}
+		}
+		a++;
+	}
+});
+
+/*
+ "text01":{
+	"text11":"好奇",
+	"class":"nav03-one",
+	"href":"../../../html/goodDetail.html"
+}
+ * */
--- js/index.js
+++ js/index.js
@@ -25,3 +25,91 @@ $.get("../data/index/footer01/footer01.json",function(data){
 	$(".footer01-img02>img").attr({alt:data[1].alt});
 	
 })
+
+/*轮播图*/
+$('.prev').stop().animate({opacity:0});//按钮消失
+$('.next').stop().animate({opacity:0});//按钮消失
+
+var liLen = $('.box li').length;
+var isNow = 1;//下标为1，第二张图片开始
+var liWidth = $('.box li').eq(0).outerWidth();
+$('.box').css("left",-$('.box li').eq(0).outerWidth());
+/*点击右按钮*/
+$('.next').click(function(){
+	isNow++;
+	if(isNow == liLen){
+		$('.box').css({left:-liWidth+"px"});//无缝滚动
+		isNow = 2;//下一张
+	}
+	tab();
+})
+/*点击右按钮*/
+$('.prev').click(function(){
+	isNow--;
+	if(isNow == -1){
+		$('.box').css({left:-liWidth*(liLen-2)+"px"});//无缝滚动
+		isNow = liLen-3;//下一张
+	}
+	tab();
+})
+
+//点击某个圆点定位
+for(var i=0;i<$('.nav div').length;i++){
+	$('.nav div').index = i;
+	$('.nav div').click(function(){
+		isNow = $(this).index()+1;
+		console.log($(this).index())
+		tab();
+	})
+}
+
+/*鼠标移上banner*/
+/*$('#banner').onmouseover = function(){
+	clearInterval($('#banner').timer);
+}*/
+$('#banner').mouseover(function(){
+	clearInterval($('#banner')[0].timer);
+	$('.prev').stop().animate({opacity:0.6});//按钮
+	$('.next').stop().animate({opacity:0.6});//按钮
+})
+/*鼠标移出banner*/
+$('#banner').mouseout(function(){
+	/*autoPlay();*/
+	$('.prev').stop().animate({opacity:0});//按钮消失
+	$('.next').stop().animate({opacity:0});//按钮消失
+})
+
+autoPlay();
+/*自动播放*/
+function autoPlay(){
+	clearInterval($('#banner')[0].timer);
+	$('#banner')[0].timer = setInterval(function(){
+		isNow++;
+		if(isNow == liLen){
+			$('.box').css("left",-liWidth+"px");
+			isNow = 2;
+		}
+		tab();
+	},2000)
+}
+
+function tab(){
+	for(var i=0;i<$('.nav div').length;i++){
+		$('.nav div').eq(i).removeClass("nav-active");//所有圆点先置为白色
+	}
+	
+	//liLen=8
+	var navIndex = 0;
+	if(isNow == 0){
+		navIndex = liLen-3;
+	}else if(isNow == liLen-1){
+		navIndex = 0;
+	}else{
+		navIndex = isNow -1;
+	}
+	
+	$('.nav div').eq(navIndex).addClass("nav-active");
+	//自动轮播
+	$('.box').stop().animate({left:-$('.box li').eq(0).outerWidth()*isNow});
+}
+
